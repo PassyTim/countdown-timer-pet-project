d75f0b2fd0adabb73fde3f2b5729cfada36480cd
PassyTim <passy.exe@gmail.com>
2024-03-09 14:20:00 +0300

    Initial commit. Added XAMl code.

---
+++ countdown-timer-pet-project/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace countdown_timer_pet_project;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
