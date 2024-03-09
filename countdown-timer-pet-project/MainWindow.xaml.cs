bd7675d3a3f2f2e723d49d60add040534262c762
PassyTim <passy.exe@gmail.com>
2024-03-09 15:37:39 +0300

    Added timer logic

--- countdown-timer-pet-project/MainWindow.xaml.cs
+++ countdown-timer-pet-project/MainWindow.xaml.cs
@@ -15,5 +15,107 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new ViewModel();
+    }
+    
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Timer _timer;
+        private int _timerValue;
+        private int _timerInput;
+
+        public ViewModel()
+        {
+            StartCommand = new CommandController(StartTimer, CanStartTimer);
+            StopCommand = new CommandController(StopTimer);
+            ResetCommand = new CommandController(ResetTimer);
+        }
+
+        public int TimerValue
+        {
+            get => _timerValue;
+            set
+            {
+                if (_timerValue != value)
+                {
+                    _timerValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int TimerInput
+        {
+            get => _timerInput;
+            set
+            {
+                if (_timerInput != value)
+                {
+                    _timerInput = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
+        public ICommand StartCommand { get; set; }
+        public ICommand StopCommand { get; set; }
+        public ICommand ResetCommand { get; set; }
+
+        private void StartTimer(object parameter)
+        {
+            _timer = new Timer(TimerCallback, null, 0, 1000);
+        }
+
+        private void StopTimer(object parameter) => _timer?.Dispose();
+
+        private void ResetTimer(object parameter)
+        {
+            _timer?.Dispose();
+            TimerValue = 0;
+        }
+
+        private bool CanStartTimer(object parameter) => TimerInput > 0;
+
+        private void TimerCallback(object state)
+        {
+            if (TimerValue >= TimerInput)
+            {
+                StopTimer(null);
+                MessageBox.Show("Сountdown completed");
+            }
+            else
+            {
+                TimerValue++;
+            }
+        }
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CommandController : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public CommandController(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
     }
 }
